3a8147b48b331e7bda61893c0ccef79093f83b75
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-12-29 18:48:43 +0530

    sort

--- formsandusereff/src/componants/PrintFrom.jsx
+++ formsandusereff/src/componants/PrintFrom.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Form } from "../componants/Form.jsx";
 import { ShowForm } from "../componants/Showform.jsx";
 import "../componants/print.css";
 import { nanoid } from "nanoid";
 export const PrintForm = () => {
   const [list, setList] = useState([]);
-  let newList = async () => {
-    let resp = await fetch("http://localhost:5000/api/form");
-    let data = await resp.json();
-    return data;
+  useEffect(() => {
+    getTodo();
+  }, []);
+  const getTodo = () => {
+    fetch("http://localhost:5000/api/form")
+      .then((d) => d.json())
+      .then((res) => {
+        setList(res);
+      });
   };
-  newList().then((e) => {
-    console.log(e)
-  });
   const handleData = async (form) => {
     const payload = {
       id: nanoid(8),
@@ -38,6 +40,7 @@ export const PrintForm = () => {
     } catch (e) {
       console.log(e);
     }
+    getTodo();
   };
   const handleDelete = async (id) => {
     setList(list.filter((list) => list.id !== id));
@@ -49,9 +52,11 @@ export const PrintForm = () => {
       },
     });
   };
-  const handleSort = ()=>{
-    
-  }
+  const handleSort = () => {
+    var newlist = list.sort((a, b) => b.Salary - a.Salary);
+    setList(newlist);
+    console.log(newlist);
+  };
   return (
     <>
       <Form getData={handleData} />
